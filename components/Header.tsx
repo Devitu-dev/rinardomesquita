c6b54e00bc87f79551de457c5ea32c07b2ab810e
victormatia <fvictor1705@gmail.com>
2026-04-07 16:22:30 -0300

    refactor: organiza importações e ajusta estilo do Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-// type TSections = 'home' | 'cadernos' | 'ritos' | 'contato' | 'sobre';
+import { useState } from 'react';
+
+type TSections = 'home' | 'cadernos' | 'ritos' | 'contato' | 'sobre';
 
 function Header() {
-  // const [section, setSection] = useState<TSections>('home');
+  const [section, setSection] = useState<TSections>('home');
   return (
-    <nav className="flex items-center justify-between py-5 px-6">
+    <nav className="flex items-center justify-between py-5 px-10 fixed w-full top-0 border-b border-black/10 bg-white/80 backdrop-blur-md">
       <a className="uppercase text-primary font-semibold text-xl hover:text-secondary" href="">
         Rinardo Mesquita
       </a>
-      <ul className="relative flex items-center justify-evenly gap-6 text-black/80 py-1">
-        <li className="hover:text-secondary cursor-pointer">
+      <ul className="flex items-center justify-evenly gap-6 text-black/80 py-1">
+        <li className="hover:text-secondary cursor-pointer relative">
           Home
-          {/* {section === 'home' && <div className="h-px w-full bg-primary absolute bottom-0" /> } */}
+          {section === 'home' && <div className="h-px w-full bg-primary absolute bottom-0" />}
         </li>
         <li className="hover:text-secondary cursor-pointer">Cadernos</li>
         <li className="hover:text-secondary cursor-pointer">Ritos</li>
