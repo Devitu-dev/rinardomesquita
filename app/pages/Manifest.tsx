5ece5d91e8d0e3cb0e04ae31cb40778f5c02a376
victormatia <fvictor1705@gmail.com>
2026-04-09 14:49:21 -0300

    refactor: melhora a semantica estrutural do componente Manifest

--- app/pages/Manifest.tsx
+++ app/pages/Manifest.tsx
@@ -2,13 +2,13 @@ import wallpaper from '@/assets/images/espiral-wallpaper.png';
 
 function Manifest() {
   return (
-    <div
+    <section
       style={{
         backgroundImage: `url(${wallpaper.src})`,
       }}
       className="flex flex-col justify-center items-center py-20 px-6">
       <div className="space-y-6 text-center max-w-xl">
-        <p className="font-normal text-black/80 text-base">Manifesto</p>
+        <span className="block leading-relaxed font-normal text-black/80 text-base">Manifesto</span>
         <h2 className="font-semibold text-3xl text-primary">
           Acolher o pensamento com profundidade, beleza e presença.
         </h2>
@@ -16,7 +16,7 @@ function Manifest() {
           Um site que não apenas informa, mas convida à pausa, ao encontro e à leitura com densidade.
         </p>
       </div>
-    </div>
+    </section>
   );
 }
 
