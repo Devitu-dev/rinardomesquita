5eea489864c9c01079535155196f37830300d4ee
victormatia <fvictor1705@gmail.com>
2026-04-16 15:34:07 -0300

    feat: finaliza layout da seção de home

---
+++ sections/Home.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import hero from '@/assets/images/Hero.png';
+
+function Home() {
+  return (
+    <section className="bg-linear-40 from-white from-30% to-gray/80 pt-32 pb-12 md:py-32 space-y-12 px-6">
+      <div className="max-w-360 mx-auto flex items-center md:justify-between justify-center flex-wrap-reverse gap-6 md:gap-0">
+        <div className="flex flex-col md:basis-1/2 w-full gap-8 items-center md:items-start">
+          <span className="text-base text-primary py-2 px-4 bg-gray border border-primary/20 rounded-full">
+            presença, pensamento e experiência
+          </span>
+          <h1 className="font-semibold text-5xl text-black">Um espaço vivo para leitura, escuta e transformação</h1>
+          <p className="text-black/80 text-xl">
+            A AMANI propõe uma experiência editorial e sensível entre filosofia, educação, espiritualidade, psicanálise
+            e arte.
+          </p>
+          <div className="flex items-center gap-8">
+            <button className="hover:opacity-80 hover:cursor-pointer transition-all bg-primary border border-primary/80 px-8 py-3 rounded-2xl font-normal text-lg text-white">
+              Conheça Rinardo
+            </button>
+            <button className="hover:bg-gray/80 hover:cursor-pointer transition-all bg-gray/25 border border-primary/25 px-8 py-3 rounded-2xl font-normal text-lg text-black/80">
+              Explorar Cadernos
+            </button>
+          </div>
+        </div>
+        <div className=" flex justify-center items-center">
+          <Image loading="eager" src={hero} alt="uma foto de Rinardo Mesquita" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
