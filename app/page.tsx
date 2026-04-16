51f6a24c2feab273f824b4e92eda097b9416bed9
victormatia <fvictor1705@gmail.com>
2026-04-16 17:23:14 -0300

    feat: adiciona seções MobileHeader, Notebooks, About e Contact ao layout da página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import Header from '@/components/Header';
-import Home from './pages/Home';
-import Manifest from './pages/Manifest';
+import MobileHeader from '@/components/MobileHeader';
+import Home from '../sections/Home';
+import Manifest from '../sections/Manifest';
+import Notebooks from '../sections/Notebooks';
+import About from '@/sections/About';
+import Contact from '@/sections/Contact';
 
 export default function HomeRoute() {
   return (
     <main className="relative">
       <Header />
+      <MobileHeader />
       <Home />
       <Manifest />
+      <Notebooks />
+      <About />
+      <Contact />
     </main>
   );
 }
