0c5f6511126c4e2a3011d72f667804dbc2bbb8dd
victormatia <fvictor1705@gmail.com>
2026-04-24 13:27:15 -0300

    feat: finaliza Manifesto

--- sections/Manifest.tsx
+++ sections/Manifest.tsx
@@ -1,4 +1,6 @@
 import wallpaper from '@/assets/images/espiral-wallpaper.png';
+import Image from 'next/image';
+import logo from '@/assets/images/logo-verde.svg';
 
 function Manifest() {
   return (
@@ -8,13 +10,13 @@ function Manifest() {
       }}
       className="flex flex-col justify-center items-center py-20 px-6">
       <div className="space-y-6 text-center max-w-xl">
-        <span className="block leading-relaxed font-normal text-black/80 text-base">Manifesto</span>
         <h2 className="font-semibold text-3xl text-primary">
           Acolher o pensamento com profundidade, beleza e presença.
         </h2>
         <p className="text-black/80 font-normal text-xl">
           Um site que não apenas informa, mas convida à pausa, ao encontro e à leitura com densidade.
         </p>
+        <Image className="mx-auto" src={logo} alt="logo" />
       </div>
     </section>
   );
