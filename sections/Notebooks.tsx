06c12e8bb10636a92610ac6b4f8aef09c9eb161d
victormatia <fvictor1705@gmail.com>
2026-04-24 13:44:53 -0300

    feat: finaliza notebooks

--- sections/Notebooks.tsx
+++ sections/Notebooks.tsx
@@ -2,10 +2,16 @@ import NotebookCategoryCard from '@/components/NotebookCategoryCard';
 import { MoveRight } from 'lucide-react';
 import { categories } from '@/data/notebooksCategories.json';
 import Link from 'next/link';
+import bg from '@/assets/images/notebooks-bg.png';
 
 function Notebooks() {
   return (
-    <section id="cadernos" className="bg-gray py-16 space-y-12 px-6">
+    <section
+      style={{
+        backgroundImage: `url(${bg.src})`,
+      }}
+      id="cadernos"
+      className="bg-right bg-no-repeat bg-cover py-16 space-y-12 px-6">
       <div className="max-w-360 mx-auto flex items-baseline-last justify-between flex-wrap gap-6 md:gap-0">
         <div className="space-y-6 text-left max-w-xl">
           <span className="block leading-relaxed font-normal text-black/80 text-xl">Cadernos</span>
@@ -23,7 +29,13 @@ function Notebooks() {
       </div>
       <ul className="flex items-baseline justify-center gap-6 pb-4 flex-wrap">
         {categories.map(({ id, title, description, cover }) => (
-          <NotebookCategoryCard key={id} title={title} description={description} cover={cover} />
+          <NotebookCategoryCard
+            key={id}
+            title={title}
+            description={description}
+            cover={cover}
+            redirectoTo={`/notebooks/categories/${id}`}
+          />
         ))}
       </ul>
     </section>
