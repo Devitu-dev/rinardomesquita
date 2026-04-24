973e6094ff9709bc93120b4395771ab2ed91bec2
victormatia <fvictor1705@gmail.com>
2026-04-24 13:17:37 -0300

    feat: finaliza Home

--- sections/Home.tsx
+++ sections/Home.tsx
@@ -1,19 +1,24 @@
 import Image from 'next/image';
 import hero from '@/assets/images/Hero.png';
 import Link from 'next/link';
+import bg from '@/assets/images/home-bg.png';
 
 function Home() {
   return (
-    <section id="home" className="bg-linear-40 from-white from-30% to-gray/80 px-6 pt-32 pb-12 space-y-12 md:py-32">
-      <div className="max-w-360 mx-auto flex items-center md:justify-between justify-center flex-wrap-reverse gap-6 md:gap-0">
+    <section
+      style={{
+        backgroundImage: `url(${bg.src})`,
+      }}
+      id="home"
+      className="min-h-dvh bg-right bg-no-repeat px-6 pt-32 pb-16 md:py-32">
+      <div className="max-w-360 mx-auto flex items-center md:justify-between justify-center flex-wrap-reverse md:gap-0">
         <div className="flex flex-col md:basis-1/2 w-full gap-8 items-center md:items-start">
           <span className="text-base text-primary py-2 px-4 bg-gray border border-primary/20 rounded-full">
             presença, pensamento e experiência
           </span>
           <h1 className="font-semibold text-5xl text-black">Um espaço vivo para leitura, escuta e transformação</h1>
           <p className="text-black/80 text-xl">
-            A AMANI propõe uma experiência editorial e sensível entre filosofia, educação, espiritualidade, psicanálise
-            e arte.
+            Uma experiência editorial e sensível entre filosofia, educação, espiritualidade e arte.
           </p>
           <div className="flex items-center gap-8">
             <Link href="#sobre">
