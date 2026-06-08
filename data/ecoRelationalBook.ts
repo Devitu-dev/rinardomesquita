373610c5e0081909c2ea739615a45faef6f081d9
victormatia <fvictor1705@gmail.com>
2026-06-08 17:16:46 -0300

    feat: adiciona dados do livro "Espiritualidade Eco-Relacional" ao ecoRelationalBook

---
+++ data/ecoRelationalBook.ts
@@ -0,0 +1,42 @@
+export const ecoRelationalBook = {
+  title: 'Espiritualidade Eco-Relacional',
+  subtitle: 'Entrelaçando saberes para a formação docente',
+  author: 'José Rinardo Alves Mesquita',
+  publisher: 'Editora CRV',
+  year: '2023',
+  pages: '300 páginas',
+  format: '16x23 cm',
+  isbn: '978-65-251-4492-4',
+  digitalIsbn: '978-65-251-4493-1',
+  doi: '10.24824/978652514492.4',
+  coverUrl:
+    'https://books.google.com/books/content?id=5KfIEAAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api',
+  purchaseUrl: 'https://www.mercadolivre.com.br/espiritualidade-eco-relacional/p/MLB43360352',
+  googleBooksUrl:
+    'https://books.google.com.br/books/about/ESPIRITUALIDADE_ECO_RELACIONAL.html?id=5KfIEAAAQBAJ&redir_esc=y',
+  publisherUrl:
+    'https://loja.editoracrv.com.br/produtos/espiritualidade-eco-relacional-entrelacando-saberes-para-a-formacao-docente',
+  description:
+    'Uma obra sobre educação, espiritualidade transdisciplinar e perspectiva eco-relacional, atravessada pela dialogicidade freireana, pela escuta ativa e pela construção coletiva de saberes.',
+  summary:
+    'O livro propõe caminhos para o reencantamento da Educação a partir de encontros sensíveis, afetividade, solidariedade, participação crítica e cuidado com a inteireza humana. A espiritualidade eco-relacional aparece como modo de ser e atuar nas escolas e em outros espaços formativos, aproximando corpo, palavra, comunidade, natureza, fé, esperança e racionalidades plurais.',
+  authorBio:
+    'Rinardo Mesquita é pedagogo, educador, ator e diretor teatral, professor da rede pública estadual do Ceará e professor universitário. Doutor em Educação pela Universidade Federal do Ceará, mestre em Sociobiodiversidade e Tecnologias Sustentáveis pela UNILAB-CE e especialista em Educação Biocêntrica, atua com formação de professoras(es), gestão educacional, arte, juventudes, espiritualidade, linguagem e psicanálise.',
+  chapters: [
+    'Prefácio',
+    'Primeiras tessituras: a teia como princípio organizador',
+    'Tecendo fios epistêmico-relacionais',
+    'Teias metodológicas formativas: tecendo caminhos e entrelaçando fios na artesania dos encontros',
+    'Encontros dialógicos eco-relacionais: reflexividade no percurso',
+    'Teia epistêmica relacional: síntese e diálogos categoriais',
+    'Considerações finais: a teia que não finda',
+  ],
+  themes: [
+    'Espiritualidade Eco-Relacional',
+    'Formação docente',
+    'Dialogicidade freireana',
+    'Educação Biocêntrica',
+    'Pedagogia do Cuidado',
+    'Experiências pedagógicas dialógicas',
+  ],
+} as const;
